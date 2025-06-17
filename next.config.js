c8de6ad10e149b588828954f8c9f32c1b3db1526
Tauheed Elahee <online-accounts@tauheed-elahee.ca>
2025-06-17 17:34:50 -0400

    Configure Babel as SWC fallback

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Disable SWC and use Babel instead for WebContainer compatibility
+  swcMinify: false,
+  compiler: {
+    // Disable SWC compiler features
+    removeConsole: false,
+  },
   images: {
     domains: ['images.pexels.com'],
   },
